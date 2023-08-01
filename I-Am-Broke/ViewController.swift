34f16248c4a60a07266efed2ce2bed26825a7e71
Martin Flischman <wipes_meshes.03@icloud.com>
2023-08-01 14:59:02 +0200

    Initial commit
    feat: Created a new project.

---
+++ I-Am-Broke/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I-Am-Broke
+//
+//  Created by Martin Flischman on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
